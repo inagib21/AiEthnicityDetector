646e683e9a11f9762fe33794c79dd2a2ec93b476
inagib21 <inagib21@gmail.com>
2025-06-02 18:34:53 -0400

    Build: Use @vercel/static-build and revised build.sh for Python API

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Exit immediately if a command exits with a non-zero status, and print each command before executing it.
 set -ex
 
-echo "BUILD.SH SCRIPT EXECUTION STARTED"
+echo "BUILD.SH (STATIC-BUILD) EXECUTION STARTED"
 
 echo "Running apt-get update..."
 apt-get update -y
@@ -18,11 +18,22 @@ echo "Verifying cmake installation..."
 which cmake
 cmake --version
 
-echo "CMAKE INFO PRINTED. BUILD.SH SCRIPT EXECUTION FINISHED."
+echo "CMAKE INFO PRINTED."
 
-# Upgrade pip and install Python dependencies
-echo "Installing Python dependencies..."
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+echo "Creating output directory structure for Python function..."
 
-echo "Python dependencies installed successfully." 
+echo "Upgrading pip..."
+python3 -m pip install --user --upgrade pip
+echo "PIP UPGRADED."
+
+echo "Installing Python dependencies from requirements.txt into the current directory..."
+python3 -m pip install --target=. -r requirements.txt
+echo "PYTHON DEPENDENCIES INSTALLATION FINISHED."
+
+if [ -d "FairFace" ]; then
+  echo "FairFace directory found."
+else
+  echo "WARNING: FairFace directory NOT found in build context!"
+fi
+
+echo "BUILD.SH (STATIC-BUILD) EXECUTION FINISHED." 
